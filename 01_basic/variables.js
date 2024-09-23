ff235ffb35b33e4353efb14ed4a5df2d3466725b
maindoliya <43407749+maindoliya@users.noreply.github.com>
2024-09-23 07:09:38 +0000

    basics of variables

---
+++ 01_basic/variables.js
@@ -0,0 +1,16 @@
+const accountID = 1234
+let userID = "manishmaindoliya@gmail.com"
+var userPassword = "12345"
+accountCity = "Jaipur"
+let userState;
+var accState;
+
+//accountID = 2 
+
+console.log(accountID)
+
+userID = "vinay@gmail.com"
+userPassword = "34535"
+accountCity = "Bengaluru"
+
+console.table([userID,userPassword,accountCity,userState,accState])
