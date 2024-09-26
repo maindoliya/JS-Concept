c85fc42028401438e9c6da551619a3cd402e14be
maindoliya <43407749+maindoliya@users.noreply.github.com>
2024-09-26 09:26:32 +0000

    basics of comparisions

---
+++ 01_basic/comparision.js
@@ -0,0 +1,20 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+
+// comparision covert null to a number , trating as 0.
+// thats why null >=0 and null <= 0 is true;
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
+
+// incosistancy in language
+
+
+// strict check (===)
