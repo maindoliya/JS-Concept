559f323c71f056a715d42f472d9b2764c417e757
maindoliya <43407749+maindoliya@users.noreply.github.com>
2024-09-26 08:04:57 +0000

    basics for data types

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,14 @@
+// number
+// bigInt
+// string
+//null = stand alone value like temprature aya hi nhe so it is null it can be 0 so 0 can not be assign
+// undefine = kuch assign nhe hua h
+// symbol => for unique
+
+// object
+
+console.log(typeof "Manish");
+
+console.log(typeof null);  /// null is object
+
+console.log(typeof undefined); // undefined type is undefined
