b0613da8cc24bf8903a0584f4e21f17793c0d3fe
maindoliya <43407749+maindoliya@users.noreply.github.com>
2024-10-02 18:02:50 +0000

    data type and strings

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,31 @@
+// premitive data type ,(stack)
+
+// 7 types
+// String ,Number ,Boolean ,null = empty (like temprature),
+//undefined(variable is declared) but kya value ayegi vo nhe pta
+// symbol(value ko unique bnane k liye)
+// BigInt
+
+
+
+//NonPremitive or refrence type ,(Heap)
+
+// Array
+// Object 
+// Functions
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+const bigNumber = 234354353434n
+console.log(bigNumber);
+
+
+
+
+// ***************************************
+// Memories
+
---
+++ 01_basic/string.js
@@ -0,0 +1,35 @@
+
+const name = 'hitesh'
+const repoCount = 50
+
+//console.log(name + repoCount + 'value');
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshHC')
+//console.log(gameName[0]);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(4));
+//console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-2,4)
+console.log(anotherString);
+
+const newString = "    hitesh123   "
+console.log(newString.trim());
+
+// replace
+// include => it will return bool value
+
+console.log(newString.includes('ho'));
+
+// split
+
+console.log(gameName.split('s'));
+
+
+
